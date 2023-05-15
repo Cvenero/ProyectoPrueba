e131f39d710cdf4d8296d5ee9396c5f7cecd59be
Gabriela <gmartellv@unsa.edu.pe>
2023-05-15 12:17:21 -0500

    Se modifcaron index.html y index.js, se eliminaron consolelog anteriores para funcionamiento

--- Lab03-pweb2/index.js
+++ Lab03-pweb2/index.js
@@ -3,13 +3,10 @@ const server = http.createServer((request, response) => {
     console.log(request.url);
     response.end("Hola mundo");
 });
-server.listen(3000);
-console.log("Escuchando en: http://localhost:3000")
 
 const path = require('path');
 const express = require('express');
 const app = express();
-app.use(express.static('pub'));
 
 app.listen(3000, () => {
     console.log("Escuchando en: http://localhost:3000")
@@ -17,5 +14,5 @@ app.listen(3000, () => {
 });
 
 app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname, '../Lab01-pweb2/index.html'));
+    response.sendFile(path.resolve(__dirname, 'index.html'));
 });

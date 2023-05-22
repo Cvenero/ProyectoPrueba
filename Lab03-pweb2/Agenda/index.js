d25afa225624b79bacd87ee02f9e09341ad227f0
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-05-22 08:43:34 -0500

    Se modifican el get para Ver

--- Lab03-pweb2/Agenda/index.js
+++ Lab03-pweb2/Agenda/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 app.use(express.static('pub'))
+app.use(express.static('css'))
 
 app.listen(3000, () => {
     console.log("Escuchando en: http://localhost:3000")
@@ -13,8 +14,8 @@ app.get('/', (request, response) => {
 })
 
 
-app.get('/recitar', (request, response) => {
-    fs.readFile(path.resolve(__dirname, 'priv/poema.txt'), 'utf8',
+app.get('/Ver', (request, response) => {
+    fs.readFile(path.resolve(__dirname, '/'), 'utf8',
         (err, data) => {
             if (err) {
                 console.error(err)

468eddc284a28d1327ba9e1c4c083b679516ae7c
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-05-28 23:04:15 -0500

    Entrando al directorio privado para ver los posibles archivos que se crearon version 2

--- Lab03-pweb2/Agenda/index5.js
+++ Lab03-pweb2/Agenda/index5.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path');
 const express = require('express');
+const { error } = require('console');
 const app = express();
 app.use(express.static('pub'));
 app.use(express.static('css'))
@@ -19,5 +20,38 @@ app.post('/crear', (request, response) => {
     const text = request.body.text;
     console.log(title,text);
 
+    const archivo= title+'.txt';
+    const rutaArchivo= path.join(__dirname, 'priv', archivo);
+    fs.writeFileSync(rutaArchivo,text,(err)=>{
+        if(err){
+            console.log("Error escribiendo archivo",err)
 
-})
+        }else{
+            console.log("Archivo creado");
+            response.send("Datos guardados exitoso");
+        }
+    });
+
+});
+
+app.get("/data",(request, response)=>{
+    console.log("entrando datos")
+    fs.readdir("priv","utf-8",(err, data)=>{
+        if(err){
+            console.error(err)
+            return
+        }
+        response.json({
+            text : data
+        })
+    })
+    console.log("salida")
+}
+
+
+
+
+
+
+)
+

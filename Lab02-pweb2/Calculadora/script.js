9103e1b844f562826a935ea521f48a896724bb5f
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-05-28 23:27:25 -0500

    Terminando la funcion de darValor al js y creando limpiar para dejar todas las variables en blanco

--- Lab02-pweb2/Calculadora/script.js
+++ Lab02-pweb2/Calculadora/script.js
@@ -2,5 +2,12 @@ let resultado = document.getElementById("resultado");
 let operacion = "";
 
 function darValor(valor){
-    
+    operacion= operacion+valor;
+    resultado.valor = operacion;
+
+}
+
+function limpiar(){
+    operacion = "";
+    resultado.valor="";
 }

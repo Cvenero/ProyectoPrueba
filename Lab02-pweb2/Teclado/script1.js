70930ee153bef6ccf8bd9f4773436f873916bd22
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-06-08 00:39:09 -0500

    Agregando la funcion conversion del nodelist de los numeros a un array

--- Lab02-pweb2/Teclado/script1.js
+++ Lab02-pweb2/Teclado/script1.js
@@ -1,6 +1,15 @@
 
 var codigoInput = document.getElementById("codigoInput");
 var enter = document.getElementById("enter");
+var botones = document.getElementsByClassName("botones");
+
+
+Array.from(botones).forEach(function(botones) {
+    botones.addEventListener("click", function() {
+      codigoInput.value += botones.innerText;
+    });
+});
+//
 
 
 

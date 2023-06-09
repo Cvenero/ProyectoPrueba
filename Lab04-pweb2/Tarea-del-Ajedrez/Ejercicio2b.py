53597eb5685b448fbe94314421eb65103f2da8d3
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-06-09 08:09:40 -0500

    Agregando Ejercicio2b, primera prueba

--- Lab04-pweb2/Tarea-del-Ajedrez/Ejercicio2b.py
+++ Lab04-pweb2/Tarea-del-Ajedrez/Ejercicio2b.py
@@ -1,2 +1,10 @@
 from interpreter import draw
 from chessPictures import *
+
+arriba = knight.join(knight.negative())
+abajo = knight.negative().join(knight)
+abajo2= abajo.horizontalMirror()
+
+img = arriba.up(abajo2)
+
+draw(img)

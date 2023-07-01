0696bf446751c7328e20591da73ac670425f2785
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-06-30 22:28:13 -0500

    Agregando metodos y ultimas modificaciones

--- Lab04-pweb2/Tarea-del-Ajedrez/Ejercicio2d.py
+++ Lab04-pweb2/Tarea-del-Ajedrez/Ejercicio2d.py
@@ -1,3 +1,12 @@
 from interpreter import draw
 from chessPictures import *
 
+imagen = square
+
+for i in range(7):
+    if (i%2 ==0):
+        imagen = imagen.join(square.negative())
+    else:
+        imagen = imagen.join(square)
+
+draw(imagen)
